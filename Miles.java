bfcbc222b0051f4cef6a82150c61ff371501352d
Vaughan Galvin <vaughan.galvin@gmail.com>
2021-01-22 16:42:58 +0300

    sample code added

---
+++ Miles.java
@@ -0,0 +1,11 @@
+public class Miles {
+            public static void main(String[] args) {
+            int counter = 20; // кратность
+
+            int price = 7000; // цена билета
+
+            int miles = price / counter; // итоговый бонус
+
+            System.out.println(miles);
+        }
+    }
